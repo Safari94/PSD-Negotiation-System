26691a49b3c8c6b2fc0ca47f579192ce0339c0d3
Xavier Passos Rodrigues <xavier@MacBook-Pro-de-Xavier.local>
2016-11-28 15:26:09 +0100

    Commit inicial

---
+++ negotiationSytem/src/main/java/Client/Client.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+/**
+ *
+ * @author Xavier Rodrigues , Antonio Anjo, João Monteiro
+ */
+public class Client {
+    
+
+    
+}
