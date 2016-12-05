fc2bb902bb0deeebbcc556e76279ffbc7f33a01b
Xavier Passos Rodrigues <xavier@MacBook-Pro-de-Xavier.local>
2016-12-05 13:09:21 +0100

    client.proto adicionado e class client com algum código

--- negotiationSytem/src/main/java/Client/Client.java
+++ negotiationSytem/src/main/java/Client/Client.java
@@ -5,11 +5,12 @@
  */
 package Client;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import com.google.protobuf.CodedInputStream;
+import com.google.protobuf.CodedOutputStream;
+
+import java.io.*;
+import java.net.*;
+
 /**
  *
  * @author Xavier Rodrigues , Antonio Anjo, João Monteiro
@@ -17,7 +18,56 @@ import java.io.OutputStreamWriter;
 public class Client {
     
     public static void main(String[] args){
-        
-    }
+       
+    try{
+        if(args.length<2)
+        System.exit(1);
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+         Socket s = new Socket(host, port);
+        CodedInputStream cis = CodedInputStream.newInstance(s.getInputStream());
+        CodedOutputStream cos = CodedOutputStream.newInstance(s.getOutputStream());
+        Person p = createPerson();
+        AddressBook a = AddressBook.newBuilder().addPerson(p).build();
+        byte[] ba = a.toByteArray();
+        while (true) {
+             System.out.println("Len: " + ba.length);
+             cos.writeRawVarint32(ba.length);
+             System.out.println("Wrote Len");
+             cos.writeRawBytes(ba);
+             System.out.println("Wrote " + ba.length + " bytes");
+             cos.flush();
+             Thread.sleep(3000);
+           }
     
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(0);
+    }
+  }
+
+  static Buy createBuy(String company, int actions, float price) {
+    return
+      Buy.newBuilder()
+      .setCompany(company)
+      .setActions(actions)
+      .setPrice(price)
+     .build();
+  }
+  
+  static Sell creatSell() {
+    return
+      Bue.newBuilder()
+      .setId(1234)
+      .setName("John Doe")
+      .setEmail("jdoe@example.com")
+      .addPhone(
+        Person.PhoneNumber.newBuilder()
+          .setNumber("555-4321")
+          .setType(Person.PhoneType.HOME))
+      .build();
+
 }
+    
+    
+
