c0bcccd319f97cf724ffc114c8c632968dfb126f
Xavier Passos Rodrigues <xavier@MBP-de-Xavier.lan>
2016-12-29 21:58:12 +0100

    last changes

--- Gradle/psdNegotiation/src/main/java/Exchange/RequestManager.java
+++ Gradle/psdNegotiation/src/main/java/Exchange/RequestManager.java
@@ -23,6 +23,8 @@ public class RequestManager extends BasicActor<Message, Void> {
         this.pedidos = new LinkedList<Pedidos>();
     }
 
+    
+
 
     @Override
     @SuppressWarnings("empty-statement")
@@ -35,9 +37,79 @@ public class RequestManager extends BasicActor<Message, Void> {
                 case SELL:
                     Sell s=(Sell) message.o;
 
+                    for(Buy b:this.buys){
+
+                        if(b.company.equals(s.company)){
+                            if(b.price>=s.price){
+                                float p= (b.price+s.price)/2;
+                                if(b.amount>=s.amount) {
+                                    pedidos.add(new Pedidos(s.username, b.username,b.company,s.amount, p));
+                                    buys.add(new Buy(b.company,(b.amount-s.amount),b.price,b.username));
+                                    buys.remove(b);
+                                }
+
+                                if(b.amount<s.amount) {
+                                    pedidos.add(new Pedidos(s.username, b.username,b.company,s.amount, p));
+                                    sells.add(new Sell(s.company,(s.amount-b.amount),s.price,s.username));
+                                    buys.remove(b);
+                                }
+
+
+
+                            }
+                        }
+
+
+
+
+
+                    }
+
                     return true;
                 case BUY:
                     Buy b=(Buy) message.o;
+
+                    for(Sell s1:this.sells){
+
+                        if(s1.company.equals(b.company)){
+                            if(s1.price>=b.price){
+                                float p= (s1.price+b.price)/2;
+                                if(b.amount>=s1.amount) {
+                                    pedidos.add(new Pedidos(s1.username, b.username,b.company,s1.amount, p));
+                                    buys.add(new Buy(b.company,(b.amount-s1.amount),b.price,b.username));
+                                    buys.remove(b);
+                                }
+
+                                if(b.amount<s1.amount) {
+                                    pedidos.add(new Pedidos(s1.username, b.username,b.company,s1.amount, p));
+                                    sells.add(new Sell(s1.company,(s1.amount-b.amount),s1.price,s1.username));
+                                    buys.remove(b);
+                                }
+
+
+
+                            }
+                        }
+
+
+
+
+
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                     return true;
 
@@ -74,4 +146,4 @@ public class RequestManager extends BasicActor<Message, Void> {
 
 
 
-}
+
