b225c9c7108f116dfb2a8ea6f795e3ffc81842c9
Xavier Passos Rodrigues <xavier@MBP-de-Xavier.lan>
2016-12-30 11:14:28 +0100

    last changes

---
+++ Gradle/psdNegotiation/src/main/java/Settlement/Server.java
@@ -0,0 +1,40 @@
+package Settlement;
+
+import org.zeromq.ZMQ;
+
+/**
+ * Created by xavier on 29/12/16.
+ */
+public class Server {
+
+    private static final ZMQ.Context context = ZMQ.context(1);
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        int port = 12347;
+        ZMQ.Socket socket = context.socket(ZMQ.SUB);
+
+        socket.connect("tcp://localhost:" + port);
+        socket.connect("inproc://noti");
+
+
+
+
+
+        while (true) {
+
+            byte[] b = socket.recv();
+            String mess = new String(b);
+
+            String[] aux = mess.split(" ");
+
+
+
+
+        }
+        //socket.close();
+        //context.term();
+    }
+}
