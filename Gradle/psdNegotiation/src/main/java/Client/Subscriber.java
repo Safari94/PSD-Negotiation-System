8aadd2688d3c51c09ea243a81f589fd82154f409
Ricardo Ribeiro Ferreira <RicardoFerreira@Ricardos-MBP.home>
2017-01-21 15:43:48 +0000

    Unsubscribe Method

--- Gradle/psdNegotiation/src/main/java/Client/Subscriber.java
+++ Gradle/psdNegotiation/src/main/java/Client/Subscriber.java
@@ -12,17 +12,21 @@ public class Subscriber {
 
 
     private static final  String loginMenu = "\n1.Login\n0.Exit\n";
-    private static final String mainMenu = "==========================\n"
+    /*private static final String mainMenu = "==========================\n"
             + "1. Notify by Enterpise.\n2. Notify by amount.\n3. Notify by price c\n"
             + "4. All\n5. I made my choice, please subscribe me.\n"
-            + "6. Show Choices\n7. Reset\n8. Logout\n\n0. Exit\n==========================\n";
+            + "6. Show Choices\n7. Reset\n8. Logout\n\n0. Exit\n==========================\n";*/
+    private static final String mainMenu = "==========================\n"
+    + "1. Subscribe to enterprise notifications.\n2. Unsubscribe from enterprise notifications.\n3. Validate my choices.\n4. List subscriptions\n"
+    + "\n5. Logout\n\n0. Exit\n==========================\n";
 
     private static Scanner input = new Scanner(System.in);
     private static Socket server               = null;
     private static BufferedReader fromServer   = null;
     private static BufferedWriter toServer     = null;
     private static boolean logged              = false;
-    private static ArrayList<String> notifTypes;
+    private static ArrayList<String> subscriptions;
+    private static ArrayList<String> unsubscriptions;
 
 
 
@@ -68,7 +72,7 @@ public class Subscriber {
 
     private static int consoleMenu(){
         String option;
-
+        String company;
 
         System.out.println("==========================\n"+mainMenu
                 +"\n==========================\n");
@@ -76,10 +80,21 @@ public class Subscriber {
 
         switch(option){
             case "1":
-                notifTypes.add("info:Enterprise");
+                System.out.println("Please Enter the enterprise Name to subscribe:");
+                company = input.nextLine();
+                subscriptions.add("info:" + company);
                 break;
             case "2":
-                notifTypes.add("info:Amount");
+                System.out.println("Please Enter the enterprise Name to unsubscribe:");
+                company = input.nextLine();
+                unsubscriptions.add("info:" + company);
+                break;
+            case "3":
+                subscribe(subscriptions, unsubscriptions);
+            case "4":
+                displayChoices(subscriptions);
+           /* case "2":
+                notifTypes.add("infou:" + company);
                 break;
             case "3":
                 notifTypes.add("info:Price");
@@ -99,6 +114,9 @@ public class Subscriber {
                 break;
             case "8":
                 logged = false;
+                return 0;*/
+            case "5":
+                logged = false;
                 return 0;
             case "0":
                 logged = false;
@@ -113,14 +131,14 @@ public class Subscriber {
     public static void displayChoices(ArrayList<String> types){
         for(String s : types){
             if (s.equals("info")){
-                System.out.println("Reauested Subscription to all notifications");
+                System.out.println("Requested Subscription to all notifications");
             }else{
-                System.out.println("Reauested Subscription to "+s);
+                System.out.println("Requested Subscription to "+s);
             }
         }
     }
     
-    public static void subscribe(ArrayList<String> types){
+    public static void subscribe(ArrayList<String> subs, ArrayList<String> unsubs){
         byte[] received;
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket socket = context.socket(ZMQ.SUB);
@@ -128,11 +146,16 @@ public class Subscriber {
 
         System.out.println("Connected");
 
-        for(String s : types){
+        for(String s : subs){
             System.out.println("Subscribing to "+s);
             socket.subscribe(s.getBytes());
         }
-        while (true) {
+        
+        for(String s : unsubs){
+            System.out.println("Unsubcribing from "+s);
+            socket.unsubscribe(s.getBytes());
+        }
+        while (!Thread.currentThread().isInterrupted()) {
             System.out.println("Entering While waiting for message Sub...");//D
             received = socket.recv();
             System.out.print(new String(received));
@@ -146,7 +169,9 @@ public class Subscriber {
         fromServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
         toServer = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
         boolean exitflag = false;
-        notifTypes = new ArrayList<>();
+        subscriptions = new ArrayList<>();
+        unsubscriptions = new ArrayList<>();
+        //notifTypes = new ArrayList<>();
         Subscriber sb= new Subscriber();
 
         System.out.println("==== Notification Console ====\nWelcome\n");
--- Gradle/psdNegotiation/src/main/java/Exchange/SubscriptionManager.java
+++ Gradle/psdNegotiation/src/main/java/Exchange/SubscriptionManager.java
@@ -6,9 +6,7 @@ import co.paralleluniverse.fibers.SuspendExecution;
 import Exchange.Message.Type;
 import java.util.HashMap;
 
-/**
- * Manages Chatroom-related requests, and sends relevant notifications to a publisher.
- */
+
 public class SubscriptionManager extends BasicActor<Message, Void> {
 
     private final HashMap<String,ActorRef> subscriptionList;
@@ -29,36 +27,37 @@ public class SubscriptionManager extends BasicActor<Message, Void> {
                 
                 ActorRef user;
                 String infoEnterprise;
-                String infoPrice;
-                String infoAmount;
-                String infoAll;
+                //String infoPrice;
+                //String infoAmount;
+                //String infoAll;
                 switch(message.type){
                     case BUY:
                         Buy buyrqst = (Buy) message.o;
-                        infoEnterprise = "info:Enterprise : Enterprise " + buyrqst.getCompany() + " buy";
-                        infoAmount = "info:Amount : Amount " + buyrqst.getAmount() + " buy";
-                        infoPrice = "info:Enterprise : Amount " + buyrqst.getPrice() + " buy";
-                        infoAll = "info : Enterprise " + buyrqst.getCompany() + " Quantity : " + buyrqst.getAmount() + " for a price of : " + buyrqst.getPrice() + " buy";
+                        infoEnterprise = "info:" + buyrqst.getCompany() + " Quantity : " + buyrqst.getAmount() + " for a price of : " + buyrqst.getPrice() + " buy";
+
+                        //infoEnterprise = "info:" + buyrqst.getCompany() + " buy";
+                        //infoAll = "info : Enterprise " + buyrqst.getCompany() + " Quantity : " + buyrqst.getAmount() + " for a price of : " + buyrqst.getPrice() + " buy";
 
 
                         publisher.send(new Message(null,infoEnterprise));
-                        publisher.send(new Message(null,infoAmount));
-                        publisher.send(new Message(null,infoPrice));
-                        publisher.send(new Message(null,infoAll));
+                        //publisher.send(new Message(null,infoAmount));
+                        //publisher.send(new Message(null,infoPrice));
+                        //publisher.send(new Message(null,infoAll));
                         
                         return true;
                         
                     case SELL:
                         Sell sellrqst = (Sell) message.o;
-                        infoEnterprise = "info:Enterprise : Enterprise " + sellrqst.getCompany() + " sell";
-                        infoAmount = "info:Amount : Amount " + sellrqst.getAmount() + " sell";
-                        infoPrice = "info:Price : Price " + sellrqst.getPrice() + " sell";
-                        infoAll = "info : Enterprise " + sellrqst.getCompany() + " Quantity : " + sellrqst.getAmount() + " for a price of : " + sellrqst.getPrice() + " sell";
+                        infoEnterprise = "info:" + sellrqst.getCompany() + " Quantity : " + sellrqst.getAmount() + " for a price of : " + sellrqst.getPrice() + " sell";
+                        //infoEnterprise = "info:Enterprise : Enterprise " + sellrqst.getCompany() + " sell";
+                        //infoAmount = "info:Amount : Amount " + sellrqst.getAmount() + " sell";
+                        //infoPrice = "info:Price : Price " + sellrqst.getPrice() + " sell";
+                        //infoAll = "info : Enterprise " + sellrqst.getCompany() + " Quantity : " + sellrqst.getAmount() + " for a price of : " + sellrqst.getPrice() + " sell";
 
                         publisher.send(new Message(null,infoEnterprise));
-                        publisher.send(new Message(null,infoAmount));
-                        publisher.send(new Message(null,infoPrice));
-                        publisher.send(new Message(null,infoAll));
+                        //publisher.send(new Message(null,infoAmount));
+                        //publisher.send(new Message(null,infoPrice));
+                        //publisher.send(new Message(null,infoAll));
                         
                         return true;
                 }
